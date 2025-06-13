6bc8468589c89e56fcf93b6466e720c85e454045
josue-quevedo <utm23010407@utma.edu.mx>
2025-06-13 17:29:01 -0600

    Primer subida

---
+++ app.js
@@ -0,0 +1,19 @@
+import mongoose from "mongoose";
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+
+dotenv.config();
+mongoose.connect(process.env.urlbd)
+.then(()=>{
+    console.log("Funciona la conexion a la base de datos")
+})
+.catch((error)=>{
+    console.log("No jalo", error)
+})
+
+const app = express();
+app.use(cors());
+app.listen(4000, ()=>{
+    console.log("Escuchando al servidor")
+})
